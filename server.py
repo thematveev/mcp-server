d5c54c17b79b2467768a96620ab742d46948c477
Matveev Maksim <thematveev21@gmail.com>
2025-09-16 16:52:15 +0300

    initialize step

--- server.py
+++ server.py
@@ -1,15 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import random
 
 app = Flask(__name__)
 
 
 def mcp_handler():
-    print(request.data)
-    return str(random.randint(0, 100))
+    data = request.json()
+    if data['method'] == "initialize":
+        return jsonify({
+          "jsonrpc": "2.0",
+          "id": data["id"],
+          "result": {
+            "protocolVersion": data["params"]["protocolVersion"],
+            "capabilities": {
+              "tools": {
+                "listChanged": False
+              },
+              "resources": {}
+            },
+            "serverInfo": {
+              "name": "flask-server",
+              "version": "1.0.1"
+            }
+          }
+        })
 
 
-app.add_url_rule('/mcp', view_func=mcp_handler, methods=["POST", "GET"])
+app.add_url_rule('/mcp', view_func=mcp_handler, methods=["POST"])
 
 if __name__ == "__main__":
     app.run()
